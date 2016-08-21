9558c86e9a0e0070d8cd6723b5d36fa6e383d774
hanxu <hanxuhx@rd.netease.com>
2016-08-21 18:56:13 +0800

    秒转成时分秒

--- RAC/RAC/ViewController.m
+++ RAC/RAC/ViewController.m
@@ -63,4 +63,15 @@
 }
 
 
+- (NSString*)TimeformatFromSeconds:(NSInteger)seconds {
+    //format of hour
+    NSString *str_hour = [NSString stringWithFormat:@"%02d",seconds/3600];
+    //format of minute
+    NSString *str_minute = [NSString stringWithFormat:@"%02d",(seconds%3600)/60];
+    //format of second
+    NSString *str_second = [NSString stringWithFormat:@"%02d",seconds%60];
+    //format of time
+    NSString *format_time = [NSString stringWithFormat:@"%@:%@:%@",str_hour,str_minute,str_second];
+    return format_time;
+}
 @end
